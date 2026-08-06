01d21e595837b26489bc9a70ca5373cf18d9ce74
YuChen Liu <103021257+liuifrec@users.noreply.github.com>
2026-08-06 16:12:49 +0900

    Export Census report helpers

--- src/cellondesk/__init__.py
+++ src/cellondesk/__init__.py
@@ -1,6 +1,7 @@
 """CellOnDesk public API."""
 
 from . import legacy_feature_compat as _legacy_feature_compat  # noqa: F401
+from .census_report import render_census_report, write_census_report
 from .expression import GeneExpressionPreview, inspect_gene_expression
 from .h5ad_compat import H5ADInspection, inspect_h5ad
 from .models import DatasetRecord
@@ -15,5 +16,7 @@ __all__ = [
     "inspect_gene_expression",
     "inspect_h5ad",
     "preview_census_gene",
+    "render_census_report",
+    "write_census_report",
 ]
-__version__ = "0.5.0"
+__version__ = "0.6.0"
