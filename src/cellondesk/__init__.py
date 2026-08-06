c0afea4fa0d68c52f65e898e3242af09ef16509e
YuChen Liu <103021257+liuifrec@users.noreply.github.com>
2026-08-06 15:25:46 +0900

    Export gene expression API

--- src/cellondesk/__init__.py
+++ src/cellondesk/__init__.py
@@ -1,7 +1,14 @@
 """CellOnDesk public API."""
 
+from .expression import GeneExpressionPreview, inspect_gene_expression
 from .inspection import H5ADInspection, inspect_h5ad
 from .models import DatasetRecord
 
-__all__ = ["DatasetRecord", "H5ADInspection", "inspect_h5ad"]
-__version__ = "0.3.0"
+__all__ = [
+    "DatasetRecord",
+    "GeneExpressionPreview",
+    "H5ADInspection",
+    "inspect_gene_expression",
+    "inspect_h5ad",
+]
+__version__ = "0.4.0"
