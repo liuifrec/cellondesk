0639bb1346c3df185f430cfb9719a21c9e119a58
YuChen Liu <103021257+liuifrec@users.noreply.github.com>
2026-08-06 15:43:12 +0900

    Install legacy gene field compatibility

--- src/cellondesk/__init__.py
+++ src/cellondesk/__init__.py
@@ -1,5 +1,6 @@
 """CellOnDesk public API."""
 
+from . import legacy_feature_compat as _legacy_feature_compat  # noqa: F401
 from .expression import GeneExpressionPreview, inspect_gene_expression
 from .h5ad_compat import H5ADInspection, inspect_h5ad
 from .models import DatasetRecord
