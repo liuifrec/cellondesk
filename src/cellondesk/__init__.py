d07058749ed565e22002703f0bc8af7202f8ca4c
YuChen Liu <103021257+liuifrec@users.noreply.github.com>
2026-08-06 16:05:15 +0900

    Expose Census preview API

--- src/cellondesk/__init__.py
+++ src/cellondesk/__init__.py
@@ -4,12 +4,16 @@ from . import legacy_feature_compat as _legacy_feature_compat  # noqa: F401
 from .expression import GeneExpressionPreview, inspect_gene_expression
 from .h5ad_compat import H5ADInspection, inspect_h5ad
 from .models import DatasetRecord
+from .sources.census import CensusGenePreview, CensusQuery, preview_census_gene
 
 __all__ = [
+    "CensusGenePreview",
+    "CensusQuery",
     "DatasetRecord",
     "GeneExpressionPreview",
     "H5ADInspection",
     "inspect_gene_expression",
     "inspect_h5ad",
+    "preview_census_gene",
 ]
-__version__ = "0.4.0"
+__version__ = "0.5.0"
